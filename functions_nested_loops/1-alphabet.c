c1d13d44e32c310c76202efd063e03c5b5bf8001
ordes <10027@holbertonstudents.com>
2024-10-24 11:43:21 +0000

    print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+
 void print_alphabet(void)
 {
 char letter;
