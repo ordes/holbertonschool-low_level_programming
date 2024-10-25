c4001aeb4b159cbe70806dbd5eb8cfd6cd8f1abc
ordes <10027@holbertonstudents.com>
2024-10-25 08:50:45 +0000

    Write a function that prints the sign of a number.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,23 +1,26 @@
 #include "main.h"
 /**
- * print_sing -  prints the sign of a number.
- * Return: 1 if positive , 0 if zero, -1 if negative
- */
+* print_sign - prints the sign of a number
+* @n: the number to check
+* Return: 1 and prints + if n is greater than zero,
+* 0 and prints 0 if n is zero,
+* -1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else
-    {
-        _putchar('-');
-        return (-1);
-    }
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
