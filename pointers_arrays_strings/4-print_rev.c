799609a2d6e110bff960bd81ea273124a02da752
ordes <10027@holbertonstudents.com>
2024-10-30 13:17:36 +0000

    added new file

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_rev - prints string in reverse
+* @s: - point to respective string
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int last_index = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		last_index = i;
+	}
+
+	if (last_index == 0)
+	{
+		return;
+	}
+
+	for (i = last_index; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
