ed80b6e9daf6635ab25aea7a8cb740d6bf2607e8
ordes <10027@holbertonstudents.com>
2024-10-30 13:23:35 +0000

    added new file

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,16 @@
 void print_rev(char *s)
 {
 	int i;
-	int last_index;
+	int last_index = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		last_index = i;
 	}
-
+	if (last_index == 0)
+	{
+		return;
+	}
 
 	for (i = last_index - 1; i >= 0; i--)
 	{
