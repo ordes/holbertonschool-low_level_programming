029c1d8904da676efaa83f11b3e4ffa3c861780b
ordes <10027@holbertonstudents.com>
2024-11-01 11:40:46 +0000

    added new file

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates n bytes
+* @dest: points to string
+* @src: points to source
+* @n: nr of bytes
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
