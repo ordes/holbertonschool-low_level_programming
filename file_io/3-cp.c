b588ac2f34eae885c61d5176378952acc7775cdf
ordes <10027@holbertonstudents.com>
2024-12-06 11:27:49 +0000

    Write a program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,72 +1,73 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * closefd - closes file descriptors
- *@fd1: first file
- *@fd2: second file
- */
-void closefd(int fd1, int fd2)
+* closefunc - closses file descriptors
+* @fd1: closes 1 file
+* @fd2: closes 2 file
+*/
+
+
+void closefunc(int fd1, int fd2)
 {
 	if (close(fd1) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
 		exit(100);
 	}
 	if (close(fd2) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
 		exit(100);
 	}
 }
 
-
 /**
- * main - Entry point
- * @argc: -number of argumenst supplied to argv
- * @argv: -array of arguments
- * Return: (0);
- */
+* main - entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
-	int fdr, fdw;
+	int fd1, fd2;
 	char buffer[1024 * 8];
-	int n, m;
+	int readed, written;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdr = open(argv[1], O_RDONLY);
-	fdw = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fdr == -1)
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (fdw == -1)
+	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-		n = read(fdr, buffer, 1024 * 8);
-		if (n == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		m = write(fdw, buffer, n);
-		if (m == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		closefd(fdr, fdw);
+	readed = read(fd1, buffer, 1024 * 8);
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	written = write(fd2, buffer, readed);
+	if (written == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	closefunc(fd1, fd2);
 	return (0);
 }
